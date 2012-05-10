45a15828bc4295e75c894fb4fabdb22c49bf559b
Jason M. Kusar <jason@kusar.net>
2012-05-10 09:47:22 +0000

    Renamed directories

--- spec/datastax_rails/base_spec.rb
+++ spec/datastax_rails/base_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe DatastaxRails::Base do
-  it "should inherit from CassandraObject::Base" do
-    DatastaxRails::Base.ancestors.should include(CassandraObject::Base)
-  end
-  
   it "should run before_save" do
     p = Person.new(:name => "Jason")
     p.save

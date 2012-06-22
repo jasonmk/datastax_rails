4a8e308cb7777eecad503e473ff181345adf6d6b
Jason M. Kusar <jason@kusar.net>
2012-06-22 20:59:19 +0000

    Test updates

--- spec/datastax_rails/attribute_methods_spec.rb
+++ spec/datastax_rails/attribute_methods_spec.rb
@@ -10,4 +10,14 @@ describe DatastaxRails::Base do
     @tester ||= AttributeMethodsTester.new
   end
   
+  describe "attribute methods" do
+    it "should create attribute setter methods" do
+      tester.should respond_to(:test_string=)
+    end
+    
+    it "Should create attribute getter methods" do
+      tester.should respond_to(:test_string)
+    end
+  end
+  
 end

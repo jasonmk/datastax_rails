0739f8f2206ac51cb1497b0bf2febe26b7d6f581
Jason M. Kusar <jason@kusar.net>
2012-07-31 21:05:32 +0000

    Add spec for Persistence module

---
+++ spec/datastax_rails/persistence_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "DatastaxRails::Base" do
+  describe "persistence" do
+    describe "#create" do
+      it "should persist at the given consistency level" do
+        DatastaxRails::Base.connection.stub(:execute_cql_query)
+        DatastaxRails::Base.connection.should_receive(:execute_cql_query).with(/USING CONSISTENCY LOCAL_QUORUM/i).and_return(true)
+        Person.create({:name => 'Steven'},{:consistency => 'LOCAL_QUORUM'})
+      end
+    end
+    
+    describe "#save" do
+      it "should persist at the given consistency level" do
+        DatastaxRails::Base.connection.stub(:execute_cql_query)
+        DatastaxRails::Base.connection.should_receive(:execute_cql_query).with(/USING CONSISTENCY LOCAL_QUORUM/i).and_return(true)
+        p=Person.new(:name => 'Steven')
+        p.save(:consistency => 'LOCAL_QUORUM')
+      end
+    end
+  end
+end

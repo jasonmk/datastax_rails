c6c78b1c9478ea84b85132481db3efad3f2b973a
Jason M. Kusar <jason@kusar.net>
2012-08-30 21:20:44 +0000

    Fix array type bug

--- lib/datastax_rails/types/array_type.rb
+++ lib/datastax_rails/types/array_type.rb
@@ -70,12 +70,12 @@ module DatastaxRails
         ar = Array(array)
         ar.uniq! if options[:unique]
         ar.join("$$$$")
-      end
 
       def decode(str)
         return [] if str.blank?
-        
-        str.is_a?(Array) ? str.flatten : str.split(/(&&&&|$$$$)/) 
+        # Temporary fix
+        str.gsub!(/&&&&/,'$$$$')
+        str.is_a?(Array) ? str.flatten : str.split(/\$\$\$\$/).reject{|a|a.blank?} 
       end
 
       def wrap(record, name, value)

451cbb1e384759eaa79cbcc4af27f1e5eab4d69d
Jason M. Kusar <jason@kusar.net>
2014-05-08 20:01:14 +0000

    Test that lists allow dups and preserve order.

--- spec/datastax_rails/types/dirty_list_spec.rb
+++ spec/datastax_rails/types/dirty_list_spec.rb
@@ -6,8 +6,14 @@ describe DatastaxRails::Types::DirtyList do
   before(:each) do
     subject << "Test String 1"
     subject << "Another Test String"
+    subject << "Test String 1"
   end
   
-  it { should eq(["Test String 1", "Another Test String"])}
+  it { should eq(["Test String 1", "Another Test String", "Test String 1"])}
   its('record.changed_attributes') { should include('list' => []) }
+  
+  it "preserves ordering" do
+    subject[1] = "Test String 2"
+    subject.should eq(["Test String 1", "Test String 2", "Test String 1"])
+  end
 end

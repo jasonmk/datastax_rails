1e1a67f3ae7fab8f407cc67eb7b5027cd13c8538
Jason M. Kusar <jason@kusar.net>
2014-09-24 14:39:08 +0000

    Override to_xml to include pagination details

--- lib/datastax_rails/collection.rb
+++ lib/datastax_rails/collection.rb
@@ -19,5 +19,18 @@ module DatastaxRails
       return 1 unless per_page
       (total_entries / per_page.to_f).ceil
     end
+
+    def to_xml(options = {})
+      unless options[:root]
+        options[:root] = if all? { |e| e.is_a?(first.class) && first.class.to_s != 'Hash' }
+                           first.class.to_s.underscore.pluralize.tr('/', '-')
+                         else
+                           'records'
+                         end
+        options[:root] += %Q( current_page="#{current_page}" per_page="#{per_page}" total_entries="#{total_entries}")
+        options[:dasherize] = false
+      end
+      super
+    end
   end
 end

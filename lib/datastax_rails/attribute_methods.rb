42c2dad2361277b61af744f27b48fcc528e4263d
Jason M. Kusar <jason@kusar.net>
2014-10-02 20:43:25 +0000

    Add missing attribute_for_inspect message

--- lib/datastax_rails/attribute_methods.rb
+++ lib/datastax_rails/attribute_methods.rb
@@ -142,6 +142,31 @@ module DatastaxRails
       self.class.column_for_attribute(name)
     end
 
+    # Returns an <tt>#inspect</tt>-like string for the value of the
+    # attribute +attr_name+. String attributes are truncated upto 50
+    # characters, and Date and Time attributes are returned in the
+    # <tt>:db</tt> format. Other attributes return the value of
+    # <tt>#inspect</tt> without modification.
+    #
+    #   person = Person.create!(name: 'David Heinemeier Hansson ' * 3)
+    #
+    #   person.attribute_for_inspect(:name)
+    #   # => "\"David Heinemeier Hansson David Heinemeier Hansson D...\""
+    #
+    #   person.attribute_for_inspect(:created_at)
+    #   # => "\"2012-10-22 00:15:07\""
+    def attribute_for_inspect(attr_name)
+      value = read_attribute(attr_name)
+
+      if value.is_a?(String) && value.length > 50
+        "#{value[0..50]}...".inspect
+      elsif value.is_a?(Date) || value.is_a?(Time)
+        %("#{value.to_s(:db)}")
+      else
+        value.inspect
+      end
+    end
+
     protected
 
     def attribute_method?(name)

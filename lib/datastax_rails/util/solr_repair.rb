d275ce74c04468ca4867a686f33313fd3399f1ee
Jason M. Kusar <jason@kusar.net>
2014-12-16 20:43:44 +0000

    Remove deprecated replacefields directive from repair solr

--- lib/datastax_rails/util/solr_repair.rb
+++ lib/datastax_rails/util/solr_repair.rb
@@ -2,12 +2,12 @@ module DatastaxRails
   module SolrRepair
     def repair_solr
       encoded = {}
-      attributes.keys.each do |column_name|
+      attribute_definitions.keys.each do |column_name|
         value = read_attribute(column_name)
         encoded[column_name.to_s] = self.class.column_for_attribute(column_name).type_cast_for_solr(value)
       end
       xml_doc = RSolr::Xml::Generator.new.add(encoded)
-      self.class.solr_connection.update(data: xml_doc, params: { replacefields: false })
+      self.class.solr_connection.update(data: xml_doc)
     end
   end
 end
